05b4883c6bd932df91e0c61339961f16a58d6bf4
Simeon Angelov <sangelov@it-economics.bg>
2019-10-10 13:24:14 +0300

    initial commit

---
+++ src/main/java/com/oceans/oceanmaster/OceanmasterApplication.java
@@ -0,0 +1,13 @@
+package com.oceans.oceanmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OceanmasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OceanmasterApplication.class, args);
+	}
+
+}
